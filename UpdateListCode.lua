fc31fa60f377070e9bd4ec5ec43d29adc7aa32e9
windshadowruins <76866049+windshadowruins@users.noreply.github.com>
2025-03-04 05:48:37 -0800

    Another set of example added

--- UpdateListCode.lua
+++ UpdateListCode.lua
@@ -19,3 +19,18 @@ end
 function Attack3_onUpdate()
     UpdateAttack()
 end
+
+-- Or you can use this one. Only choose one of them
+
+-- Update
+function UpdateAnime()
+    if AttackCommonFunction("W_AttackRightLight1", "W_AttackRightHeavy1Start", "W_AttackLeftLight1",
+        "W_AttackLeftHeavy1", "W_AttackBothLight1", "W_AttackBothHeavy1Start", FALSE, TRUE, 0) == TRUE then
+        return
+    end
+end
+
+-- Custom Anime Handler
+function AnimeNameHere_onUpdate() UpdateAnime() end
+function AnimeNameHere_onUpdate() UpdateAnime() end
+function AnimeNameHere_onUpdate() UpdateAnime() end
